558b170d3bc86f3631eff1898ed28c7954dc2847
chocolithm <kys8336@gmail.com>
2025-03-20 14:55:00 +0900

    csv

--- src/writeCsv.ts
+++ src/writeCsv.ts
@@ -1,7 +1,7 @@
 import { writeCsvFormatFakeData } from "./fake";
 import { getFileNameAndNumber } from "./utils/getFileNameAndNumber";
 
-const [filename, numberOfFakeItems] = getFileNameAndNumber("data/fake", 1);
+const [filename, numberOfFakeItems] = getFileNameAndNumber("data/fake", 10000);
 const csvFilename = `${filename}-${numberOfFakeItems}.csv`;
 writeCsvFormatFakeData(csvFilename, numberOfFakeItems)
   .then(result => console.log(result))
